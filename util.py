67eddcf32dd4fa0b12acb0fc316055f1f2e6d49b
RadekRo <r.roclawski@ankara.pl>
2023-04-22 11:30:02 +0200

    Shortened time for a datewithout seconds

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import string
 from datetime import datetime 
 
 def get_current_date():
-    date_and_time_without_seconds = str(datetime.now())[0:19]
+    date_and_time_without_seconds = str(datetime.now())[0:16]
     return date_and_time_without_seconds
 
 def get_unique_file_name():
